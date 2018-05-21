8cee35f2485ba0cf2d1f133e5004c8d4ecdc0ced
Pedro Plowman <pedrofp@me.com>
2018-05-21 17:01:10 +1000

    Create Xcode project

---
+++ R-Name S/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  R-Name S
+//
+//  Created by Pedro Plowman on 21/5/18.
+//  Copyright © 2018 p2made. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ R-Name S/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  R-Name S
+//
+//  Created by Pedro Plowman on 21/5/18.
+//  Copyright © 2018 p2made. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
